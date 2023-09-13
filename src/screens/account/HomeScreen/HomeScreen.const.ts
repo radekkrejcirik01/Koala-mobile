d8780de12cf069a6939a7cd879c68b735bdfc023
radekkrejcirik <radekkrejcirik@Radeks-MacBook-Air.local>
2023-09-13 14:59:45 +0200

    feat: new record data

--- src/screens/account/HomeScreen/HomeScreen.const.ts
+++ src/screens/account/HomeScreen/HomeScreen.const.ts
@@ -18,19 +18,19 @@ export const DATA: RecordItem[] = [
     {
         id: 3,
         emotion: 'No motivation',
-        description: 'I am not motivated AT ALL',
+        description: 'I have no motivation AT ALL',
         tip1: 'Think of all good things that will come if you keep going',
         tip2: 'Take a whole day off'
     },
     {
-        id: 5,
+        id: 4,
         emotion: 'Tired',
-        description: 'I am so tired',
-        tip1: 'Take a nap 😴🛌',
+        description: 'I am so f tired',
+        tip1: 'Take that nap 🛌😴',
         tip2: 'Take a break, day off, week off'
     },
     {
-        id: 4,
+        id: 5,
         emotion: 'Morning anxiety',
         description: 'I woke up with an anxiety, it physically hurts',
         tip1: 'A morning walk outside with fresh air is greatly helpful',
@@ -39,9 +39,9 @@ export const DATA: RecordItem[] = [
     {
         id: 6,
         emotion: 'Overwhelmed',
-        description: 'I am overwhelmed',
+        description: 'My head is overwhelmed',
         tip1: 'Focus only on one important thing',
-        tip2: 'Asking for a help is not shameful'
+        tip2: 'Ask for a help, it is not shameful'
     },
     {
         id: 7,
@@ -51,53 +51,74 @@ export const DATA: RecordItem[] = [
         tip2: 'Have a chat with coworker about not important stuff'
     },
     {
-        id: 9,
-        emotion: 'Frustrated',
-        description: '🤬🤬🤬😡😡😡😤😤😤',
+        id: 8,
+        emotion: 'Angry',
+        description: '🤬😤🤬🤬😡😡😤😤🤬',
         tip1: `Say 'Fuck' 10 times in row`,
         tip2: 'Call you friend and spill the tea'
     },
     {
-        id: 11,
+        id: 9,
         emotion: 'Want to cry',
         description: 'I want to cry',
         tip1: `Cry, it's so okay`,
         tip2: 'Go through your photo gallery'
     },
     {
-        id: 12,
+        id: 10,
+        emotion: 'Social anxiety',
+        description: `There is too much people around me and I don't like it`,
+        tip1: 'Put on headphones and pretend you are somewhere else',
+        tip2: 'Remember that everybody are thinking only about themselves'
+    },
+    {
+        id: 11,
         emotion: 'Crying',
         description: 'I am crying',
         tip1: `Keep crying, it's so okay`,
         tip2: 'Go through your photo gallery'
     },
     {
-        id: 14,
+        id: 12,
+        emotion: 'Sad',
+        description: 'I am just sad',
+        tip1: 'Listening to your favorite music',
+        tip2: 'Watching tiktoks'
+    },
+    {
+        id: 13,
         emotion: `Can't eat`,
         description: `I can't eat`,
         tip1: 'Having a piece of a fruit is soo good and healing',
         tip2: 'Talk about it with person you trust the best'
     },
     {
-        id: 15,
+        id: 14,
         emotion: 'Existential crisis',
         description: 'I have an existential crisis',
         tip1: `Focus on one skill you enjoy the best. If you don't have one yet put finding it as purpose. It will turn the life into happy place and money will come with it`,
         tip2: 'Travel, seriously, it opens world you have no idea that exists'
     },
     {
-        id: 16,
+        id: 15,
         emotion: 'Today is ass',
         description: 'Today is ass, sucks, fucking terrible',
         tip1: 'Call your friend and tell them everything',
         tip2: 'Have your guilty pleasure, whatever it is'
     },
     {
+        id: 16,
+        emotion: 'Depressive episode',
+        description: 'I have a depressive episode',
+        tip1: 'Calling your friend',
+        tip2: 'Listening to happy or sad music'
+    },
+    {
         id: 17,
         emotion: 'Want go home',
         description: 'I WANT TO GO HOME',
         tip1: 'Put your headphones on and pretend you are somewhere else',
-        tip2: `Think of how you will reward yourself when you finally come home`
+        tip2: 'Think of how you will reward yourself when you finally come home'
     },
     {
         id: 18,
