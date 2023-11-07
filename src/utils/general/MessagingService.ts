28d134084d9f2449b2e1bfaf3860a56376b7c4df
radekkrejcirik <radekkrejcirik@Radeks-MacBook-Air.local>
2023-11-07 13:09:30 +0100

    fix: android notification permission

--- src/utils/general/MessagingService.ts
+++ src/utils/general/MessagingService.ts
@@ -1,4 +1,6 @@
+import { Platform } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
+import { check, PERMISSIONS, request, RESULTS } from 'react-native-permissions';
 import { postRequest } from '@utils/Axios/Axios.service';
 import { ResponseInterface } from '@interfaces/response/Response.interface';
 import { DevicePostInterface } from '@interfaces/post/Post.interface';
@@ -24,12 +26,45 @@ class MessagingServiceClass {
         });
     };
 
-    public initMessaging = async () => {
-        const status = await messaging().requestPermission();
+    private getToken = () => {
+        this.getDeviceToken().catch();
+        this.onTokenRefresh();
+    };
 
-        if (status === 1) {
-            this.getDeviceToken().catch();
-            this.onTokenRefresh();
+    public initMessaging = async () => {
+        if (Platform.OS === 'ios') {
+            const status = await messaging().requestPermission();
+
+            if (status === 1) {
+                this.getToken();
+            }
+        }
+
+        if (Platform.OS === 'android') {
+            const requestNotificationPermission = async () => {
+                const result = await request(
+                    PERMISSIONS.ANDROID.POST_NOTIFICATIONS
+                );
+                return result;
+            };
+
+            const checkNotificationPermission = async () => {
+                const result = await check(
+                    PERMISSIONS.ANDROID.POST_NOTIFICATIONS
+                );
+                return result;
+            };
+
+            const checkPermission = await checkNotificationPermission();
+            if (checkPermission !== RESULTS.GRANTED) {
+                const permissionStatus = await requestNotificationPermission();
+
+                if (permissionStatus === RESULTS.GRANTED) {
+                    this.getToken();
+                }
+            } else {
+                this.getToken();
+            }
         }
     };
 }
