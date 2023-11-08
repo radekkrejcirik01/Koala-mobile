eb6fc7b75534ef565d5f1070afbecd37c6601fb8
radekkrejcirik <radekkrejcirik@Radeks-MacBook-Air.local>
2023-11-08 10:52:51 +0100

    refactor: remove notification liked prop

--- src/interfaces/general.interface.ts
+++ src/interfaces/general.interface.ts
@@ -12,7 +12,6 @@ export interface NotificationInterface {
     name: string;
     message: string;
     type: NotificationItemEnum;
-    liked?: number;
     time: number;
     seen: number;
     conversationId?: number;
