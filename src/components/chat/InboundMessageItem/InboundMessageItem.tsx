82ab2b7c550f3dc822b40085224ba52a29c2e900
radekkrejcirik <radekkrejcirik@Radeks-MacBook-Air-2.local>
2024-05-31 08:22:24 +0200

    refactor: isTextEmoji function

--- src/components/chat/InboundMessageItem/InboundMessageItem.tsx
+++ src/components/chat/InboundMessageItem/InboundMessageItem.tsx
@@ -3,7 +3,7 @@ import { Text, TouchableOpacity, View } from 'react-native';
 import { InboundMessageItemProps } from '@components/chat/InboundMessageItem/InboundMessageItem.props';
 import { InboundMessageItemStyle } from '@components/chat/InboundMessageItem/InboundMessageItem.style';
 import { AudioMessageItem } from '@components/chat/AudioMessageItem/AudioMessageItem';
-import { REGEX } from '@constants/REGEX';
+import { isTextEmoji } from '@functions/isTextEmoji';
 
 export const InboundMessageItem = ({
     name,
@@ -12,50 +12,44 @@ export const InboundMessageItem = ({
     replyMessage,
     audioMessage,
     isFirst
-}: InboundMessageItemProps): React.JSX.Element => {
-    function isEmojiOnly(str: string): boolean {
-        return REGEX.test(str);
-    }
-
-    return (
-        <View
-            style={[
-                InboundMessageItemStyle.container,
-                isFirst && InboundMessageItemStyle.marginTop
-            ]}
-        >
-            {isFirst && (
-                <Text style={InboundMessageItemStyle.nameText}>{name}</Text>
+}: InboundMessageItemProps): React.JSX.Element => (
+    <View
+        style={[
+            InboundMessageItemStyle.container,
+            isFirst && InboundMessageItemStyle.marginTop
+        ]}
+    >
+        {isFirst && (
+            <Text style={InboundMessageItemStyle.nameText}>{name}</Text>
+        )}
+        <View style={InboundMessageItemStyle.messageContainer}>
+            {!!replyMessage && (
+                <View style={InboundMessageItemStyle.replyMessageView}>
+                    <Text style={InboundMessageItemStyle.replyMessageText}>
+                        {replyMessage}
+                    </Text>
+                </View>
             )}
-            <View style={InboundMessageItemStyle.messageContainer}>
-                {!!replyMessage && (
-                    <View style={InboundMessageItemStyle.replyMessageView}>
-                        <Text style={InboundMessageItemStyle.replyMessageText}>
-                            {replyMessage}
-                        </Text>
-                    </View>
-                )}
-                {audioMessage ? (
-                    <AudioMessageItem audioMessage={audioMessage} />
-                ) : (
-                    <TouchableOpacity
-                        activeOpacity={1}
-                        delayLongPress={100}
-                        hitSlop={10}
-                        onLongPress={onLongPress}
+            {audioMessage ? (
+                <AudioMessageItem audioMessage={audioMessage} />
+            ) : (
+                <TouchableOpacity
+                    activeOpacity={1}
+                    delayLongPress={100}
+                    hitSlop={10}
+                    onLongPress={onLongPress}
+                >
+                    <Text
+                        style={[
+                            InboundMessageItemStyle.messageText,
+                            isTextEmoji(children) &&
+                                InboundMessageItemStyle.largeText
+                        ]}
                     >
-                        <Text
-                            style={[
-                                InboundMessageItemStyle.messageText,
-                                isEmojiOnly(children) &&
-                                    InboundMessageItemStyle.largeText
-                            ]}
-                        >
-                            {children}
-                        </Text>
-                    </TouchableOpacity>
-                )}
-            </View>
+                        {children}
+                    </Text>
+                </TouchableOpacity>
+            )}
         </View>
-    );
-};
+    </View>
+);
--- src/components/chat/OutboundMessageItem/OutboundMessageItem.tsx
+++ src/components/chat/OutboundMessageItem/OutboundMessageItem.tsx
@@ -4,7 +4,7 @@ import { OutboundMessageItemProps } from '@components/chat/OutboundMessageItem/O
 import { OutboundMessageItemStyle } from '@components/chat/OutboundMessageItem/OutboundMessageItem.style';
 import { AudioMessageItem } from '@components/chat/AudioMessageItem/AudioMessageItem';
 import { InboundMessageItemStyle } from '@components/chat/InboundMessageItem/InboundMessageItem.style';
-import { REGEX } from '@constants/REGEX';
+import { isTextEmoji } from '@functions/isTextEmoji';
 
 export const OutboundMessageItem = ({
     children,
@@ -12,53 +12,42 @@ export const OutboundMessageItem = ({
     replyMessage,
     audioMessage,
     isFirst
-}: OutboundMessageItemProps): React.JSX.Element => {
-    function isEmojiOnly(str: string): boolean {
-        return REGEX.test(str);
-    }
-
-    return (
-        <View
-            style={[
-                OutboundMessageItemStyle.container,
-                isFirst && OutboundMessageItemStyle.marginTop
-            ]}
-        >
-            {isFirst && (
-                <Text style={OutboundMessageItemStyle.nameText}>You</Text>
+}: OutboundMessageItemProps): React.JSX.Element => (
+    <View
+        style={[
+            OutboundMessageItemStyle.container,
+            isFirst && OutboundMessageItemStyle.marginTop
+        ]}
+    >
+        {isFirst && <Text style={OutboundMessageItemStyle.nameText}>You</Text>}
+        <View style={InboundMessageItemStyle.messageContainer}>
+            {!!replyMessage && (
+                <View style={OutboundMessageItemStyle.replyMessageView}>
+                    <Text style={OutboundMessageItemStyle.replyMessageText}>
+                        {replyMessage}
+                    </Text>
+                </View>
             )}
-            <View style={InboundMessageItemStyle.messageContainer}>
-                {!!replyMessage && (
-                    <View style={OutboundMessageItemStyle.replyMessageView}>
-                        <Text style={OutboundMessageItemStyle.replyMessageText}>
-                            {replyMessage}
-                        </Text>
-                    </View>
+            <TouchableOpacity
+                activeOpacity={1}
+                delayLongPress={100}
+                hitSlop={10}
+                onLongPress={onLongPress}
+            >
+                {audioMessage ? (
+                    <AudioMessageItem audioMessage={audioMessage} outbound />
+                ) : (
+                    <Text
+                        style={[
+                            OutboundMessageItemStyle.messageText,
+                            isTextEmoji(children) &&
+                                OutboundMessageItemStyle.largeText
+                        ]}
+                    >
+                        {children}
+                    </Text>
                 )}
-                <TouchableOpacity
-                    activeOpacity={1}
-                    delayLongPress={100}
-                    hitSlop={10}
-                    onLongPress={onLongPress}
-                >
-                    {audioMessage ? (
-                        <AudioMessageItem
-                            audioMessage={audioMessage}
-                            outbound
-                        />
-                    ) : (
-                        <Text
-                            style={[
-                                OutboundMessageItemStyle.messageText,
-                                isEmojiOnly(children) &&
-                                    OutboundMessageItemStyle.largeText
-                            ]}
-                        >
-                            {children}
-                        </Text>
-                    )}
-                </TouchableOpacity>
-            </View>
+            </TouchableOpacity>
         </View>
-    );
-};
+    </View>
+);
