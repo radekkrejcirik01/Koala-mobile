3ca391d875775eaed89c520df8cf2107691eeb34
radekkrejcirik <radekkrejcirik@Radeks-MacBook-Air-2.local>
2024-06-10 08:52:31 +0200

    feat: chat margin

--- src/components/chat/ChatList/ChatList.style.ts
+++ src/components/chat/ChatList/ChatList.style.ts
@@ -3,8 +3,7 @@ import DIMENSIONS from '@constants/DIMENSIONS';
 
 export const ChatListStyle = StyleSheet.create({
     container: {
-        marginLeft: 18,
-        marginRight: 12,
+        marginHorizontal: 12,
         paddingBottom: Platform.OS === 'ios' ? DIMENSIONS.height / 2.5 : 20
     }
 });
