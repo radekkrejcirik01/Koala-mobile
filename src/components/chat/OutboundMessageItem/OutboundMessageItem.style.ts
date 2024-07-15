26f794086fa787d198a2af1d1ddc5deb87a5df6b
radekkrejcirik <radekkrejcirik@Radeks-MacBook-Air-2.local>
2024-07-15 11:38:03 +0200

    feat: reply message font size

--- src/components/chat/OutboundMessageItem/OutboundMessageItem.style.ts
+++ src/components/chat/OutboundMessageItem/OutboundMessageItem.style.ts
@@ -28,7 +28,6 @@ export const OutboundMessageItemStyle = StyleSheet.create({
         alignSelf: 'flex-end'
     },
     replyMessageText: {
-        fontSize: 15,
         color: COLORS.GRAY_200,
         fontWeight: '500'
     },
