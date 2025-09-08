d586b6ada8febd9ad05a0eaeaad26b25c219fe59
radekkrejcirik <radek.krejcirik01@gmail.com>
2025-09-08 20:43:44 +0200

    feat: add full swipe back

--- src/navigation/RootNavigator/RootStackNavigator.options.ts
+++ src/navigation/RootNavigator/RootStackNavigator.options.ts
@@ -1,12 +1,16 @@
 import { StackNavigationOptions } from '@react-navigation/stack';
+import { Dimensions } from 'react-native';
 import { transitionConfig } from './RootStackNavigator.config';
+
+const { width } = Dimensions.get('screen');
 
 export const NavigatorScreenOptions: StackNavigationOptions = {
   transitionSpec: {
     open: transitionConfig,
     close: transitionConfig
   },
-  headerTitleAlign: 'center'
+  headerTitleAlign: 'center',
+  gestureResponseDistance: width
 };
 
 export const NoHeader: StackNavigationOptions = {
