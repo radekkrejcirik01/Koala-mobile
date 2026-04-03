7b74f16162310b8df6d13e44f078f4cf4fd6d03a
radekkrejcirik <radek.krejcirik01@gmail.com>
2026-04-03 15:20:38 +0200

    chore: upgrade RN to 0.78.0

--- android/app/src/main/java/app/com/koala/MainActivity.java
+++ android/app/src/main/java/app/com/koala/MainActivity.java
@@ -1,41 +1,31 @@
-package app.com.koala;
+package app.com.koala
 
-import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-import android.os.Bundle;
-import com.zoontek.rnbootsplash.RNBootSplash;
+import android.os.Bundle
+import com.zoontek.rnbootsplash.RNBootSplash
 
-public class MainActivity extends ReactActivity {
+class MainActivity : extends ReactActivity() {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  @Override
-  protected String getMainComponentName() {
-    return "Koala";
-  }
+  override fun getMainComponentName(): String = "Koala"
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-   * (aka React 18) with two boolean flags.
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  override fun createReactActivityDelegate(): createReactActivityDelegate = {
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
   }
 
-  @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      RNBootSplash.init(this, R.style.BootTheme); // ⬅️ initialize the splash screen
-      super.onCreate(savedInstanceState); // super.onCreate(null) with react-native-screens
+  override fun onCreate(savedInstanceState: Bundle?) {
+      RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
+      super.onCreate(savedInstanceState)
     }
 }
